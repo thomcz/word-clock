f6fb9a0048b8604be2e096563a3146f24dd70de2
thomcz <thomas.czogalik@gmx.de>
2018-08-18 07:27:47 -0900

    added gol

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 from wordclock import WordClock
 from plugins.countdown import Countdown
+from plugins.gameoflife import GameOfLife
 
 from strip.ledstrip import LedStrip
 from time import sleep
@@ -8,8 +9,9 @@ from neopixel import *
 
 if __name__ == "__main__":
     print('started')
-    Countdown(Color(200, 43, 157), LedStrip()).run()
-    #wordclock = WordClock(LedStrip(), Color(255, 255, 255))
-    #while(True):
-    #    wordclock.run()
-    #    sleep(30)
+#    GameOfLife(LedStrip()).run()
+#    Countdown(Color(200, 43, 157), LedStrip()).run()
+    wordclock = WordClock(LedStrip(), Color(255, 255, 255))
+    while(True):
+        wordclock.run()
+        sleep(30)
--- python/plugins/gameoflife.py
+++ python/plugins/gameoflife.py
@@ -1,5 +1,6 @@
 from random import randint
 import strip.ledconfig as ledconfig
+from time import sleep
 
 from neopixel import *
 
@@ -8,35 +9,40 @@ class GameOfLife():
         self.strip = strip
         self.state = None
         self.simulation = Simulation()
-    
+
     def run(self):
         if self.state is None:
             self.state = self.__getRandomState()
-        self.__draw()
-        self.state = self.simulation.update(self.state)
+        while(True):
+            self.__draw()
+            self.state = self.simulation.update(self.state)
+            sleep(1)
 
     def __getRandomState(self):
         state = State()
-        for position in range(ledconfig.LED_COUNT):
-            isLiving = randint(0, 1) == 1
-            color = self.__getRandomColor()
-            cell = Cell(position, isLiving, color)
-            state.state.append(cell)
+        ledPosition = 0
+        for x in range(11):
+            for y in range(10):
+                isLiving = randint(0, 1) == 1
+                color = self.__getRandomColor()
+                cell = Cell(x, y, ledPosition, isLiving, color)
+                state.state.append(cell)
+                ledPosition += 1
         return state
 
     def __getRandomColor(self):
-        return Color(255, 255, 255)
-    
+        return Color(randint(0, 255), randint(0, 255), randint(0, 255))
+
     def __draw(self):
         leds = []
-        for i in range(self.state.state):
-            if self.state.state[i].isLiving:
-                leds.append(i)
-        self.strip.setLeds(leds, Color(255, 255, 255))
+        for i in self.state.state:
+            if i.isLiving:
+                self.strip.setLed(i.ledPosition, i.color)
+            else:
+                self.strip.setLed(i.ledPosition, 0)
 
 
 
-        
 
 class Simulation():
     def __init__(self):
@@ -45,43 +51,61 @@ class Simulation():
     def update(self, state):
         newState = State()
         for position in range(ledconfig.LED_COUNT):
-            
-            isLiving = False
-            #color = state[position].color
-            color = Color(255, 255, 255)
 
-            if state.isLiving:
-                isLiving = self__dieMaybe(state, position)
+            isLiving = False
+            actualCell = state.state[position]
+            color = actualCell.color
+
+            if actualCell.isLiving:
+                isLiving = self.__dieMaybe(state, actualCell)
             else:
-                isLiving = self.__resurection(state, position)
-            cell = Cell(position, isLiving, color)
-            newState.state.append(cell)
+                isLiving = self.__resurection(state, actualCell)
+            newCell = Cell(actualCell.x, actualCell.y, position, isLiving, color)
+            newState.state.append(newCell)
 
         return newState
+	
+    def __dieMaybe(self, state, cell):
+        neighbours =  self.__countLivingNeighbours(state, cell)
+        return neighbours == 2 or neighbours == 3
 
-    def __dieMaybe(self, state, position):
-        return False
+    def __resurection(self, state, cell):
+        return self.__countLivingNeighbours(state, cell) == 3
+		
+    def __countLivingNeighbours(self, state, cell):
+        livingNeighbours = 0
+        for i in range(ledconfig.LED_COUNT):
+            actualCell = state.state[i]
+            if actualCell.x == cell.x + 1 and actualCell.y == cell.y - 1 and actualCell.isLiving:
+                livingNeighbours += 1
+            elif actualCell.x == cell.x and actualCell.y == cell.y - 1 and actualCell.isLiving:
+                livingNeighbours += 1
+            elif actualCell.x == cell.x - 1 and actualCell.y == cell.y - 1 and actualCell.isLiving:
+                livingNeighbours += 1
+            elif actualCell.x == cell.x + 1 and actualCell.y == cell.y and actualCell.isLiving:
+                livingNeighbours += 1
+            elif actualCell.x == cell.x - 1 and actualCell.y == cell.y and actualCell.isLiving:
+                livingNeighbours += 1
+            elif actualCell.x == cell.x - 1 and actualCell.y == cell.y + 1 and actualCell.isLiving:
+                livingNeighbours += 1
+            elif actualCell.x == cell.x and actualCell.y == cell.y + 1 and actualCell.isLiving:
+                livingNeighbours += 1
+            elif actualCell.x == cell.x + 1 and actualCell.y == cell.y + 1 and actualCell.isLiving:
+                livingNeighbours += 1
 
-    def __isLiving(self, state, position):
-        return False
-
-
-class Draw():
-    def draw(state):
-        leds = []
-        for i in range(state.state):
-            if state.state[i].isLiving:
-                leds.append(i)
-        strip.setLeds(leds, Color(255, 255, 255))
-    
-
+        return livingNeighbours
 
 class State():
     def __init__(self):
         self.state = []
 
 class Cell():
-    def __init__(self, position, isLiving, color):
-        self.position = position
+    def __init__(self, x, y, ledPosition, isLiving, color):
+        self.x = x
+        self.y = y
+        self.ledPosition = ledPosition
         self.isLiving = isLiving
         self.color = color
+
+
+
